7eebb3f2e1a10d09867ffb1b23300bdde66ee486
Shanzhen <shanzhen@m2.lan>
2025-08-01 17:20:17 +0800

    Support for multiple signers on one box using different path

--- restart.sh
+++ restart.sh
@@ -1,7 +1,8 @@
 . ./venv/bin/activate
 pip install -r requirements.txt
 sudo setcap 'cap_net_bind_service=+ep' $(readlink -f $(which python3))
-if tmux has-session -t run 2>/dev/null; then
-    tmux kill-session -t run
+current_path=$(pwd | tr -d '/')
+if tmux has-session -t $current_path 2>/dev/null; then
+    tmux kill-session -t $current_path
 fi
-tmux new-session -s run 'python3 sign_server.py run'
+tmux new-session -s $current_path 'python3 sign_server.py run'
