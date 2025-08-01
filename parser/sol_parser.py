ddffb416ee8df67ca706a0e99a1db82061c6d43d
Shanzhen <shanzhen@m2.lan>
2025-08-01 16:32:41 +0800

    Added support for leverage on JLP. Fixed restart script.

--- parser/sol_parser.py
+++ parser/sol_parser.py
@@ -2,6 +2,8 @@ from shield.shield import SHIELD
 from solders.pubkey import Pubkey
 from solders.keypair import Keypair
 from solana.transaction import Transaction
+from solders.transaction import VersionedTransaction
+from solders import message
 
 USDC_MINT = Pubkey.from_string("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v") # USDC Token address
 JLP_MINT = Pubkey.from_string("27G8MtK7VtTcCHkpASjSDdkWWYfoqT6ggEuKidVJidD4") # JLP Token address
@@ -21,14 +23,20 @@ def get_associated_token_address(wallet: Pubkey, mint: Pubkey) -> Pubkey:
 
 def check_transaction(transaction, function, tokenaddress=""):
     
-    t = Transaction.deserialize(bytes.fromhex(transaction))
-    
     # 只允许两种交易
     # Only two types of transactions are allowed
-    if function not in ["jlp_mint_or_burn_sign", "transfer"]:
+    if function not in ["jlp_mint_or_burn_sign", "transfer", "jlp_loan_deposit_sign_init",
+                        "jlp_loan_deposit_sign", "jlp_loan_borrow_sign",
+                        "jlp_loan_repay_sign", "jlp_loan_withdraw_sign"
+    ]:
         return {
             "error": "Invalid function"
         }
+        
+    if function == "jlp_loan_deposit_sign_init":
+        t = VersionedTransaction.from_json(transaction)
+    else:
+        t = Transaction.deserialize(bytes.fromhex(transaction))
     
     # 校验JLP mint的transation是否正确
     # Check if the JLP mint transaction is correct
@@ -103,12 +111,56 @@ def check_transaction(transaction, function, tokenaddress=""):
                 "error": "Invalid signer address"
             }
                  
+    # 校验JLP loan的transation是否正确
+    if function in ["jlp_loan_deposit_sign", "jlp_loan_borrow_sign", "jlp_loan_repay_sign", "jlp_loan_withdraw_sign"]:
+        if len(t.instructions) != 3:
+            return {
+                "error": "Invalid jlp_loan_sign instructions len"
+            }
+        
+        # 校验是否是mint或burn
+        # check if it is mint or burn
+        if not t.instructions[2].data.hex().startswith("1102c3be4c10ee4a") and not t.instructions[2].data.hex().startswith("99b741417121f92d") \
+            and not t.instructions[2].data.hex().startswith("d3dbb7def84a051a") and not t.instructions[2].data.hex().startswith("75a03c52ede92eb6"):
+            return {
+                "error": "Invalid jlp_loan_sign"
+            }
+        
+        # 校验program_id
+        # check program_id
+        if str(t.instructions[2].program_id) != "PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu" or \
+              str(t.instructions[0].program_id) != "ComputeBudget111111111111111111111111111111" or \
+                str(t.instructions[1].program_id) != "ComputeBudget111111111111111111111111111111":
+            return {
+                "error": "Invalid jlp_loan_sign program_id"
+            }
+    
+    if function == "jlp_loan_deposit_sign_init":
+        if len(t.message.instructions) != 3:
+            return {
+                "error": "Invalid jlp_loan_deposit_sign_init instructions len"
+            }
+        account_keys = t.message.account_keys
+        program_id1 = account_keys[t.message.instructions[0].program_id_index]
+        program_id2 = account_keys[t.message.instructions[1].program_id_index]
+        program_id3 = account_keys[t.message.instructions[2].program_id_index]
+        if str(program_id1) != "ComputeBudget111111111111111111111111111111" or str(program_id2) != "ComputeBudget111111111111111111111111111111" or str(program_id3) != "PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu":
+            return {
+                "error": "Invalid jlp_loan_deposit_sign_init program_id"
+            }
+        
+        if not t.message.instructions[-1].data.hex().startswith("1102c3be4c10ee4a"):
+            return {
+                "error": "Invalid jlp_loan_deposit_sign_init data"
+            }
+            
+            
     return {
         "status": "ok"
     }
 
 
-def sign_transaction(transaction, void_keypair):
+def sign_transaction(transaction, void_keypair, function):
     with SHIELD.get_private_key() as pk:
         if not pk:
             return {
@@ -117,17 +169,24 @@ def sign_transaction(transaction, void_keypair):
     
         wallet_keypair = Keypair.from_base58_string(pk)
         
-        t = Transaction.deserialize(bytes.fromhex(transaction))
-        # 某些transation需要一个额外的临时签名
-        # some transactions need an extra signature
-        if void_keypair:
-            void_keypair = Keypair.from_json(void_keypair)
-            t.sign(wallet_keypair, void_keypair)
+        if function == "jlp_loan_deposit_sign_init":
+            raw_transaction = VersionedTransaction.from_json(transaction)
+            signature = wallet_keypair.sign_message(message.to_bytes_versioned(raw_transaction.message))
+            return {
+                "result": bytes(signature).hex()
+            }
         else:
-            t.sign(wallet_keypair)
-            
-        signed_tx = t.serialize()
+            t = Transaction.deserialize(bytes.fromhex(transaction))
+            # 某些transation需要一个额外的临时签名
+            # some transactions need an extra signature
+            if void_keypair:
+                void_keypair = Keypair.from_json(void_keypair)
+                t.sign(wallet_keypair, void_keypair)
+            else:
+                t.sign(wallet_keypair)
+                
+            signed_tx = t.serialize()
 
-        return {
-            "result": signed_tx.hex()
-        }
+            return {
+                "result": signed_tx.hex()
+            }
--- restart.sh
+++ restart.sh
@@ -1,5 +1,7 @@
 . ./venv/bin/activate
 pip install -r requirements.txt
 sudo setcap 'cap_net_bind_service=+ep' $(readlink -f $(which python3))
-tmux kill-window -t run:0
+if tmux has-session -t run 2>/dev/null; then
+    tmux kill-session -t run
+fi
 tmux new-session -s run 'python3 sign_server.py run'
